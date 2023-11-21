006aa9205f798886c1c8e2a03776df6238c82b4b
Itsaida <113546067+Itsaida@users.noreply.github.com>
2023-11-21 22:42:47 +0330

    add js file

---
+++ src/index.js
@@ -0,0 +1,27 @@
+let imageConteinerEl = document.querySelector(".container");
+let previewEl = document.querySelector("#preview");
+let nextEl = document.querySelector("#next");
+
+let x = 0;
+let timer;
+previewEl.addEventListener("click", () => {
+  x = x + 45;
+  clearTimeout(timer);
+  updateGallery();
+});
+
+nextEl.addEventListener("click", () => {
+  x -= 45;
+  clearTimeout(timer);
+  updateGallery();
+});
+
+function updateGallery() {
+  imageConteinerEl.style.transform = `perspective(1000px) rotateY(${x})`;
+
+  timer = setTimeout(() => {
+    x = x - 45;
+    updateGallery();
+  }, 3000);
+}
+updateGallery();
